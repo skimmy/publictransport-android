68c54a9160848dd9a0470cba7040b4b5722d6ca9
Michele Schimd <michele.schimd@gmail.com>
2013-06-20 15:21:57 +0200

    Added OpneStreetMap tile provider

--- src/com/skimmy/publictransit/fragments/MapFragment.java
+++ src/com/skimmy/publictransit/fragments/MapFragment.java
@@ -1,9 +1,16 @@
 package com.skimmy.publictransit.fragments;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import android.os.Bundle;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Tile;
+import com.google.android.gms.maps.model.TileOverlayOptions;
+import com.google.android.gms.maps.model.TileProvider;
+import com.google.android.gms.maps.model.UrlTileProvider;
 
 public class MapFragment extends SupportMapFragment {
 	
@@ -22,6 +29,30 @@ public class MapFragment extends SupportMapFragment {
 		// TODO Auto-generated method stub
 		super.onActivityCreated(savedInstanceState);
 		this.gMap = getMap();
+		
+		TileOverlayOptions tOpts = new TileOverlayOptions().tileProvider(new OSMTileProvider());
+//		this.gMap.setMapType(GoogleMap.MAP_TYPE_NONE);
+		this.gMap.setTrafficEnabled(true);
+//		this.gMap.addTileOverlay(tOpts);
+	}
+	
+	private class OSMTileProvider extends UrlTileProvider {
+		
+		public OSMTileProvider() {
+			super(256,256);
+		}
+
+		@Override
+		public URL getTileUrl(int x, int y, int zoom) {
+			try {
+				return new URL("http://a.tile.openstreetmap.org/" + zoom + "/" + x + "/" + y + ".png");
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return null;
+		}
+		
 	}
 
 }
